169b90506dd708593d01b819c2e3d2887be84277
Prakash <prakashkl.crossovernepal.com>
2014-08-26 12:23:54 +0545

    Updated

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENT
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
